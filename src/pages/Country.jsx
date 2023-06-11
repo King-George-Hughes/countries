8e8cc39444b78d2a16ea8ea701170fe9889df51c
unknown <hughesgeorgeking@gmail.com>
2023-06-11 10:42:25 +0000

    changes

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -9,27 +9,27 @@ const Country = ({ darkMode }) => {
   const [country, setCountry] = useState([]);
   const { countryName } = useParams();
 
-  const fectCountryByName = async () => {
-    setLoading(true);
-
-    try {
-      const response = await fetch(
-        `https://restcountries.com/v2/name/${countryName}`
-      );
-      //   const response = await fetch(
-      //     `https://restcountries.com/v3.1/name/${countryName}`
-      //   );
-      const data = await response.json();
-      setLoading(false);
-      setCountry(data[0]);
-      console.log(data[0]);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
+    const fectCountryByName = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v2/name/${countryName}`
+        );
+        //   const response = await fetch(
+        //     `https://restcountries.com/v3.1/name/${countryName}`
+        //   );
+        const data = await response.json();
+        setLoading(false);
+        setCountry(data[0]);
+        console.log(data[0]);
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+      }
+    };
+
     fectCountryByName();
   }, [countryName]);
 
@@ -63,13 +63,13 @@ const Country = ({ darkMode }) => {
           to={"/"}
           className={`${
             darkMode ? "bg-darkModeElements" : "bg-white"
-          } px-10 py-2 inline-flex items-center gap-3 shadow-md rounded-md`}
+          } px-10 mt-10 py-2 inline-flex items-center gap-3 shadow-md rounded-md`}
         >
           <BsArrowLeft />
           Back
         </Link>
 
-        <div className="w-full mt-10 flex flex-col items-center justify-center md:flex-row">
+        <div className="w-full h-full mt-10 flex flex-col items-center justify-center md:flex-row">
           <div className="w-full md:w-1/2">
             <img src={flags.svg} alt="" />
           </div>
@@ -98,7 +98,7 @@ const Country = ({ darkMode }) => {
                 </h5>
                 <h5 className="text-sm my-1">
                   <span className="font-bold">Capital: </span>
-                  {capital}
+                  {capital ? capital : "NA"}
                 </h5>
               </div>
               <div className="w-full md:w-1/2">
@@ -108,7 +108,7 @@ const Country = ({ darkMode }) => {
                 </h5>
                 <h5 className="text-sm my-1">
                   <span className="font-bold">Sub Region: </span>
-                  {currencies[0].name}
+                  {currencies ? currencies[0].name : "NA"}
                 </h5>
                 <h5 className="text-sm my-1">
                   <span className="font-bold">Languages: </span>
